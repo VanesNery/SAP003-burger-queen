5d0e0e19021037071fe555356a612a27ad110e87
vanesnery <nery_van@hotmail.com>
2019-12-18 00:20:18 -0300

    Funçao de Menu ok e Rota Ok

--- src/components/button.js
+++ src/components/button.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 function Button (props){
   return (
-  <button class= {props.class} onclick={props.handleClick}>{props.title}</button>
+  <button class= {props.class} 
+  onclick={props.handleClick}>{props.title}</button>
   )
 };
 
